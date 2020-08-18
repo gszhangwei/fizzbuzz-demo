e5e312f9acaba876c6cb5dcad2891fc329778a60
Willie Zhang <wwdzhang@thoughtworks.com>
2020-08-18 15:47:18 +0800

    the first refactor for the code

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,18 +2,30 @@ import exception.NonPositiveNumberException;
 
 public class FizzBuzz {
     public String covert(int number) throws NonPositiveNumberException {
-        if(number < 0) {
+        if(isNegativeNum(number)) {
             throw new NonPositiveNumberException();
         }
-        if(number % 3 == 0 && number % 5 == 0) {
+        if(isMultipleOf3(number) && isMultipleOf5(number)) {
             return "FizzBuzz";
         }
-        if(number % 3 == 0) {
+        if(isMultipleOf3(number)) {
             return "Fizz";
         }
-        if(number % 5 == 0) {
+        if(isMultipleOf5(number)) {
             return "Buzz";
         }
         return null;
+    }
+
+    private boolean isMultipleOf5(int number) {
+        return number % 5 == 0;
+    }
+
+    private boolean isMultipleOf3(int number) {
+        return number % 3 == 0;
+    }
+
+    private boolean isNegativeNum(int number) {
+        return number < 0;
     }
 }
